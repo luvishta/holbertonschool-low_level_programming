2db25a63214bc69167cec6d58156e63400b17136
luvishta <luvishta14@gmail.com>
2026-04-15 12:41:53 +0000

    tep 10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 0 to 9
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+
+	return (0);
+}
