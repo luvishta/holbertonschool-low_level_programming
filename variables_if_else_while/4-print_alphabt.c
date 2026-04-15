5ba4e36ad5377ad9e737cbf064bdc3dbc01f73ee
luvishta <luvishta14@gmail.com>
2026-04-15 12:36:36 +0000

    tep 9

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 
+/**
+ * main - prints alphabet except e and q
+ *
+ * Return: 0
+ */
 int main(void)
 {
-    char c;
+	char c;
 
-    for (c = 'a'; c <= 'z'; c++)
-        if (c != 'e' && c != 'q')
-            putchar(c);
-    putchar('\n');
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+			putchar(c);
+	}
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
