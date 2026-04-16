50800d2f1f0533eb1c0e9736e70fc36efaced07e
luvishta <luvishta14@gmail.com>
2026-04-16 13:03:11 +0000

    tep 21

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -10,38 +10,40 @@ void print_to_98(int n)
 	{
 		if (n > 98)
 		{
-			if (n >= 100)
-				_putchar((n / 100) + '0');
-			if (n >= 10)
-				_putchar(((n / 10) % 10) + '0');
-			if (n < 10 && n > -10)
-				_putchar(n + '0');
-			else
-				_putchar((n % 10 < 0 ? -(n % 10) : n % 10) + '0');
-
-			_putchar(',');
-			_putchar(' ');
-			n--;
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
 		}
 		else
 		{
-			if (n <= -100)
-				_putchar('-'), _putchar((-n / 100) + '0');
-			if (n <= -10)
-				_putchar('-'), _putchar(((-n) / 10 % 10) + '0');
-			if (n > -10 && n < 10)
+			if (n < 10 && n >= 0)
 				_putchar(n + '0');
-			else if (n >= 10)
-				_putchar(((n / 10) % 10) + '0');
-			_putchar((n % 10 < 0 ? -(n % 10) : n % 10) + '0');
-
-			_putchar(',');
-			_putchar(' ');
-			n++;
+			else if (n < 0)
+			{
+				_putchar('-');
+				if (-n < 10)
+					_putchar((-n) + '0');
+				else
+				{
+					_putchar(((-n) / 10) + '0');
+					_putchar(((-n) % 10) + '0');
+				}
+			}
+			else
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
 		}
+
+		_putchar(',');
+		_putchar(' ');
+
+		if (n > 98)
+			n--;
+		else
+			n++;
 	}
 
-	/* print 98 */
 	_putchar('9');
 	_putchar('8');
 	_putchar('\n');
