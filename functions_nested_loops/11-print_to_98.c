ae3ac923d425a290bdd12042e7199c07d13c72c3
luvishta <luvishta14@gmail.com>
2026-04-16 13:00:36 +0000

    tep 21

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -10,19 +10,39 @@ void print_to_98(int n)
 	{
 		if (n > 98)
 		{
-			print_number(n);
+			if (n >= 100)
+				_putchar((n / 100) + '0');
+			if (n >= 10)
+				_putchar(((n / 10) % 10) + '0');
+			if (n < 10 && n > -10)
+				_putchar(n + '0');
+			else
+				_putchar((n % 10 < 0 ? -(n % 10) : n % 10) + '0');
+
 			_putchar(',');
 			_putchar(' ');
 			n--;
 		}
 		else
 		{
-			print_number(n);
+			if (n <= -100)
+				_putchar('-'), _putchar((-n / 100) + '0');
+			if (n <= -10)
+				_putchar('-'), _putchar(((-n) / 10 % 10) + '0');
+			if (n > -10 && n < 10)
+				_putchar(n + '0');
+			else if (n >= 10)
+				_putchar(((n / 10) % 10) + '0');
+			_putchar((n % 10 < 0 ? -(n % 10) : n % 10) + '0');
+
 			_putchar(',');
 			_putchar(' ');
 			n++;
 		}
 	}
-	print_number(98);
+
+	/* print 98 */
+	_putchar('9');
+	_putchar('8');
 	_putchar('\n');
 }
