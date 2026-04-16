b44bb82829ce8cc2968e9d7de1a5e215bc466114
luvishta <luvishta14@gmail.com>
2026-04-16 12:22:57 +0000

    tep 18

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,11 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-int _islower(int c);
-int _isalpha(int c);
-int _abs(int n);
-int print_sign(int n);
-int print_last_digit(int n);
+void _putchar(char c);
+void jack_bauer(void);
 
 #endif
