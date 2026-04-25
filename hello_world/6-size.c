dceb534f927bd8ae98cb11e1f9b88efe5797d7e6
luvishta <luvishta14@gmail.com>
2026-04-25 19:31:03 +0000

    heck 5

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,9 +1,9 @@
-# include <studio.h>
+#include <stdio.h>
 
 /**
- * main - Entry points
+ * main - Entry point
  *
- * return : Always o (sucess)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
