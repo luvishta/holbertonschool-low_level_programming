6b09ac24ed64a3f8f01342dc943f760219ee0b73
luvishta <luvishta14@gmail.com>
2026-05-01 16:38:10 +0000

    test 9

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -4,7 +4,9 @@
  * print_sign - prints the sign of a number
  * @n: input number
  *
- * Return: 1 if n is greater than zero, 0 if n is zero, -1 if n is less than zero
+ * Return: 1 if n is greater than zero
+ *         0 if n is zero
+ *        -1 if n is less than zero
  */
 int print_sign(int n)
 {
