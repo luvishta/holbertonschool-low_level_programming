cd073f93e430f710ee210144088501701c2770c0
luvishta <luvishta14@gmail.com>
2026-05-01 17:08:28 +0000

    test 13

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,24 +1,18 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minutes of the day from 00:00 to 
- */
-void jack_bauer(void);
-#include "main.h"
-
-/**
  * jack_bauer - prints every minute of the day
  */
 void jack_bauer(void)
 {
-	int a, m;
+	int h, m;
 
-	for (a = 0; a < 24; a++)
+	for (h = 0; h < 24; h++)
 	{
 		for (m = 0; m < 60; m++)
 		{
-			_putchar((a / 10) + '0');
-			_putchar((a % 10) + '0');
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
 			_putchar(':');
 			_putchar((m / 10) + '0');
 			_putchar((m % 10) + '0');
