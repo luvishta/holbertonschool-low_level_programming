6570f4a9e7fbd7992f3a9cf7b2b57a2f87736173
luvishta <luvishta14@gmail.com>
2026-05-01 15:43:57 +0000

    test 5

--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - check code
  *
- * Return: Always 0.
+ * Return: Always 0
  */
 int main(void)
 {
 	print_alphabet();
-
 	return (0);
 }
