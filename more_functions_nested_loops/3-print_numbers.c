b354b1a3a1c41f7fadb31e90737574d967e94adc
luvishta <luvishta14@gmail.com>
2026-05-03 16:03:58 +0000

    tep 7

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_numbers - prints number from 0 to 9
+ * print_numbers - prints numbers from 0 to 9
  *
  * Return: void
  */
-void print_numbers(void);
+void print_numbers(void)
 {
 	int i;
 
