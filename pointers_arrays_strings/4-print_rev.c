c59c3ffae955b187a96b7a23b7256622af514c00
luvishta <luvishta14@gmail.com>
2026-05-08 09:25:49 +0400

    task 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "string.h"
+/**
+*print_rev - prints string in rev followed by a new line
+*@s: check the strings
+*/
+void print_rev(char *s)
+{
+int length;
+
+for (length = 0; s[length] != 0; length++)
+{
+/*checks for end */
+}
+for (length--; length > 0; length--)
+{
+_putchar(s[length]);
+}
+_putchar('\n');
+}
