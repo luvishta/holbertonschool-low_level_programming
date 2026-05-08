803fdef3ce108ecf4e3c49d9d47bf3f059fb667a
luvishta <luvishta14@gmail.com>
2026-05-08 09:34:14 +0400

    task 4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -8,11 +8,11 @@ void print_rev(char *s)
 {
 int length;
 
-for (length = 0; s[length] != 0; length++)
+for (length = 0; s[length] != '\0'; length++)
 {
 /*checks for end */
 }
-for (length--; length > 0; length--)
+for (length--; length >= 0; length--)
 {
 _putchar(s[length]);
 }
