e1a9465bde71fe3c3018fee7edd25d23f4fa6881
luvishta <luvishta14@gmail.com>
2026-05-11 11:18:01 +0400

    task 1

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*_strcat - copies a string from src to dest
+*@dest: destination buffer
+*@src: source string
+*Return: pointer to the resulting  dest
+*/
+char *_strcat(char *dest, char *src)
+{
+int a = 0;
+int b = 0;
+while (dest[a] != '\0')
+{	
+	a++;
+}
+while (src[b] != '\0')
+{
+	dest[a] = src[b];
+	a++;
+	b++;
+	dest[a] = '\0';
+}
+return (dest);
+}
