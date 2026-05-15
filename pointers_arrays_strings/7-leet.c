dbd1bc671a18995287ce1ee696dbe90f6cd3adce
luvishta <luvishta14@gmail.com>
2026-05-15 12:22:46 +0400

    task 10

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+*leet - encodes a string into 1337
+*@s: string to be modified
+*Return: pointer to string
+*/
+char *leet(char *s)
+{
+	char a[11]  = "aAeEoOtTlL";
+	char b[11] = "4433007711";
+	int i;
+	int j;
+
+
+	for (i = 0; s[i] != '\0'; i++)
+{
+	j = 0;
+
+	for (j = 0; a[j] != '\0'; j++)
+	{
+		if (s[i] == a[j])
+		{
+			(s[i] = b[j]);
+		}
+	}
+}
+return (s);
+}
+
+
+
