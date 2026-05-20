068c87cc13d2c172d3e71ecbf247f828d96a210d
luvishta <luvishta14@gmail.com>
2026-05-20 19:58:05 +0400

    task 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -19,21 +19,26 @@ char *str_concat(char *s1, char *s2)
 
 	if (s2 == NULL)
 		s2 = "";
-
 	for (length1 = 0; s1[length1] != '\0'; length1++)
-
-		for (length2 = 0; s2[length2] != '\0'; length2++)
-
+	{
+	}
+	for (length2 = 0; s2[length2] != '\0'; length2++)
+	{
+	}
 			concat = malloc(sizeof(char) * (length1 + length2 + 1));
 
 	if (concat == NULL)
 		return (NULL);
 
 	for (i = 0; i < length1; i++)
+	{
 		concat[i] = s1[i];
+	}
 
 	for (j = 0; j < length2; j++)
+	{
 		concat[i + j] = s2[j];
+	}
 
 	concat[i + j] = '\0';
 
