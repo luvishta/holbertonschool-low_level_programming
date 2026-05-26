7823b15bbb53e86172cd7e904b62874655188958
luvishta <luvishta14@gmail.com>
2026-05-26 11:30:25 +0400

    task o

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+*struct dog - create a new type
+*@name : the name of the dog
+*@age: the age of the dog
+*@owner: the owner of the dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
