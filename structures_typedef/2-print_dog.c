4a3cd731b6164268b255b44fc08eac34518ea2a7
luvishta <luvishta14@gmail.com>
2026-05-27 09:10:04 +0400

    task 2

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+
+/**
+ * print_dog - prints the information about a dog
+ * @d: pointer to the dog structure
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		printf("(Nil)\n");
+	else
+		printf("name:%s\n", d->name);
+
+	printf("Age: %f\n", d->age);
+
+	if (d->owner == NULL)
+		printf("(nil)\n");
+	else
+		printf("Owner:%s\n", d->owner);
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
-
+#include "stdio.h"
+#include <stdlib.h>
 /**
 *struct dog - create a new type
 *@name : the name of the dog
@@ -15,4 +16,5 @@ struct dog
 
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif
