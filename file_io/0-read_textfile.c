d93ce27d2ff101a5387a1cb61f7cfd1be1c6a5b1
luvishta <luvishta14@gmail.com>
2026-06-24 10:11:45 +0400

    task 0

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+*read_textfile - reads a text file and prints it to the POSIX
+*@filename:name of file to read
+*@letters:num of letters to print and read
+*Return: filename is NULL return 0 and actual num of letters printed
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t r, w;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+
+	if (letters == 0)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(letters);
+	if (buf == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	r = read(fd, buf, letters);
+	if (r == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+
+	w = write(1, buf, r);
+
+	free(buf);
+	close(fd);
+
+	if (w != r)
+		return (0);
+
+	return (r);
+}
