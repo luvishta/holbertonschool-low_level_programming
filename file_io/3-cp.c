64e4f28b04934cfb9f5931c2c2bc8bb4911a0825
luvishta <luvishta14@gmail.com>
2026-06-26 14:32:54 +0400

    task 3

---
+++ file_io/3-cp.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+*main - copies a file into another file
+*@c: argument count
+*@v: argument vector*
+*Return: 0 on success, or exit codes on failure
+ */
+int main(int c, char *v[])
+{
+	int f1, f2, r, w;
+	char b[1024];
+
+	if (c != 3)
+	{
+		dprintf(1, "Usage: cp file_from file_to\n");
+		return (97);
+	}
+
+	f1 = open(v[1], O_RDONLY);
+	if (f1 == -1)
+		return (dprintf(1, "Error: Can't read from file %s\n", v[1]), 98);
+	f2 = open(v[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (f2 == -1)
+		return (dprintf(1, "Error: Can't write to %s\n", v[2]), close(f1), 99);
+	r = read(f1, b, 1024);
+	while (r > 0)
+	{
+		w = write(f2, b, r);
+		if (w != r)
+			return (dprintf(1, "Error: Can't write to %s\n", v[2]),
+					close(f1), close(f2), 99);
+		r = read(f1, b, 1024);
+	}
+
+	if (r == -1)
+		return (dprintf(1, "Error: Can't read from file %s\n", v[1]),
+				close(f1), close(f2), 98);
+
+	if (close(f1) == -1)
+		return (dprintf(1, "Error: Can't close fd %d\n", f1), 100);
+
+	if (close(f2) == -1)
+		return (dprintf(1, "Error: Can't close fd %d\n", f2), 100);
+	return (0);
+}
